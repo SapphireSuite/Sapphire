1335d52cf11a1c4609c4fa58e32d80a28f2929f0
mrouffet <maximerouffet@gmail.com>
2019-08-13 00:38:18 +0200

    [Add] Core:: Int Types
    Add Engine defined int types.

--- Samples/UnitTest/main.cpp
+++ Samples/UnitTest/main.cpp
@@ -2,7 +2,7 @@
 
 #include "UnitTest.hpp"
 
-#include <Sapphire/Core/Types/Char.hpp>
+#include <Sapphire/Core/Types/Int.hpp>
 using namespace Sa;
 
 int main()
@@ -10,28 +10,38 @@ int main()
 	LOG("=== Start ===");
 
 
-	LOG("\n=== _c8 ===");
-	SA_TEST(120_c8, ==, 120);
-	SA_TEST(-117_c8, ==, -117);
-	SA_TEST(0x10FE52D9B864FA37_c8, ==, (char)0x37);
+	LOG("\n=== _i8 / _ui8 ===");
+	SA_TEST(120_i8, ==, 120);
+	SA_TEST(120_ui8, ==, 120u);
+	SA_TEST(-117_i8, ==, -117);
+	SA_TEST(0x10FE52D9B864FA37_ui8, ==, (char)0x37);
 
 
-	LOG("\n=== _c16 ===");
-	SA_TEST(120_c16, ==, 120u);
-	SA_TEST(-117_c16, ==, -117);
-	SA_TEST(0x10FE52D9B864FA37_c16, ==, 0xFA37u);
+	LOG("\n=== _i16 / _ui16 ===");
+	SA_TEST(120_i16, ==, 120);
+	SA_TEST(120_ui16, ==, 120u);
+	SA_TEST(-117_i16, ==, -117);
+	SA_TEST(0x10FE52D9B864FA37_ui16, ==, 0xFA37u);
 
 
-	LOG("\n=== _c32 ===");
-	SA_TEST(120_c32, ==, 120u);
-	SA_TEST(-117_c32, ==, -117u);
-	SA_TEST(0x10FE52D9B864FA37_c32, ==, 0xB864FA37u);
+	LOG("\n=== _i32 / _ui32 ===");
+	SA_TEST(120_i32, ==, 120);
+	SA_TEST(120_ui32, ==, 120u);
+	SA_TEST(-117_i32, ==, -117);
+	SA_TEST(0x10FE52D9B864FA37_ui32, ==, 0xB864FA37u);
+
+	LOG("\n=== _i64 / _ui64 ===");
+	SA_TEST(120_i64, ==, 120);
+	SA_TEST(120_ui64, ==, 120u);
+	SA_TEST(-117_i64, ==, -117);
+	SA_TEST(0x10FE52D9B864FA37_ui64, ==, 0x10FE52D9B864FA37LLU);
 
 
-	LOG("\n=== CharOfSize ===");
-	SA_TEST(sizeof(CharOfSize<1>), ==, sizeof(char));
-	SA_TEST(sizeof(CharOfSize<2>), ==, sizeof(char16_t));
-	SA_TEST(sizeof(CharOfSize<4>), ==, sizeof(char32_t));
+	LOG("\n=== IntOfSize ===");
+	SA_TEST(sizeof(IntOfSize<1>), ==, sizeof(char));
+	SA_TEST(sizeof(IntOfSize<2>), ==, sizeof(short int));
+	SA_TEST(sizeof(IntOfSize<4>), ==, sizeof(int));
+	SA_TEST(sizeof(IntOfSize<8>), ==, sizeof(long long int));
 
 
 	LOG("\n=== End ===");
