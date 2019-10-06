fa4b38c4a204c5ecf6a967a19d166a6fb159ab52
mrouffet <maximerouffet@gmail.com>
2019-10-06 13:11:53 +0200

    [Add] Collections:: Thread
    Add grouped includes for all Core/Thread/*.hpp

--- Samples/UnitTest/main.cpp
+++ Samples/UnitTest/main.cpp
@@ -2,8 +2,7 @@
 
 #include "UnitTest.hpp"
 
-#include <Sapphire/Core/Thread/Atomic.hpp>
-#include <Sapphire/Core/Thread/Thread.hpp>
+#include <Sapphire/Collections/Thread>
 using namespace Sa;
 
 int main()
@@ -11,54 +10,7 @@ int main()
 	LOG("=== Start ===");
 
 
-	Atomic<bool> waitForOther = true;
 
-	uint32 count = 0u;
-
-	Thread th1([&waitForOther, &count]()
-	{
-		while (waitForOther)
-			Thread::Yield();
-
-		waitForOther = true;
-
-		for (uint32 i = 0u; i < 5u; ++i)
-		{
-			++count;
-			LOG("Th1");
-		}
-
-		waitForOther = false;
-	});
-
-	Thread th2([&waitForOther, &count]()
-	{
-		while (waitForOther)
-			Thread::Yield();
-
-		waitForOther = true;
-
-		for (uint32 i = 0u; i < 5u; ++i)
-		{
-			++count;
-			LOG("Th2");
-		}
-
-		waitForOther = false;
-	});
-
-	LOG("Th1 ID: " << th1.GetID());
-	LOG("Th2 ID: " << th2.GetID());
-
-	SA_TEST(th1.GetID(), != , 0u);
-	SA_TEST(th2.GetID(), !=, 0u);
-
-	waitForOther = false;
-
-	th1.Join();
-	th2.Join();
-
-	SA_TEST(count, ==, 10u);
 
 
 	LOG("\n=== End ===");
