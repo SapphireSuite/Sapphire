fa6f8e70839319078f81ba8279dae288af396df9
mrouffet <maximerouffet@gmail.com>
2020-04-24 12:12:38 +0200

    Add Core:: Pragma directive and Windows.h support

---
+++ Engine/Include/Sapphire/Core/Support/Windows.hpp
@@ -0,0 +1,37 @@
+// Copyright 2020 Sapphire development team. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_CORE_WINDOWS_GUARD
+#define SAPPHIRE_CORE_WINDOWS_GUARD
+
+#include <Core/Support/Pragma.hpp>
+#include <Core/Support/Platforms.hpp>
+
+/**
+*	\file Windows.hpp
+*
+*	\brief Sapphire's support of Windows.h file.
+*
+*	\ingroup Core
+*	\{
+*/
+
+#if SA_WIN
+
+	/**
+	*	Remove macro producing 'define' expansion warning.
+	*	See: https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/c5105
+	*/
+	SA_PRAGMA_SDWARN_MSVC(5105)
+
+	#include <Windows.h>
+
+	SA_PRAGMA_EDWARN()
+
+#endif
+
+
+/** \} */
+
+#endif // GUARD
